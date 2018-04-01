83c49e37fce6be9f7d7736468857c6f91d5d69e7
Todd Dukart <tdukart@mechanicalninja.com>
2018-04-01 10:20:38 -0400

    Convert to pure TypeScript

--- src/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "string-arrays-equal" {
-    function stringArraysEqual(arrayA: string[], arrayB: string[]): boolean;
-
-    export = stringArraysEqual;
-}
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,28 @@
 // webpack.config.js
 
 var config = {
-	entry: __dirname + '/src/index.js',
-	devtool: 'source-map',
-	output: {
-		path: __dirname + '/dist',
-		filename: 'index.js',
-		library: 'string-arrays-equal',
-		libraryTarget: 'umd',
-		umdNamedDefine: true
-	},
-	module: {
-		loaders: [
-			{
-				test: [ /\.js?$/ ],
-				exclude: /node_modules/,
-				loader: 'babel-loader',
-				query: {
-					presets: [ 'env' ],
-				},
-			},
-		],
-	},
-	resolve: {
-		extensions: [ '.js' ]
-	}
+  entry: __dirname + '/src/index.ts',
+  devtool: 'inline-source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js',
+    library: 'string-arrays-equal',
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: [/\.tsx?$/],
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 };
 
 module.exports = config;
